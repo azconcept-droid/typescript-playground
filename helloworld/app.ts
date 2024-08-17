213ca76a9aa42415a8bb429196c5837a0b2470ad
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-17 10:55:49 +0100

    add an interface

--- helloworld/app.ts
+++ helloworld/app.ts
@@ -6,3 +6,28 @@ heading.textContent = message;
 
 // add the heading to document
 document.body.appendChild(heading)
+
+interface Product{
+    id: number,
+    name: string,
+    price: number
+}
+
+function getProduct(id: number) : Product{
+    return {
+        id: id,
+        name: `Awesome Gadget ${id}`,
+        price: 99.5
+    }
+}
+
+const showProduct = (name: string, price: number) => {
+    console.log(`The product ${name} costs $${price}`);
+}
+
+const product = getProduct(1);
+showProduct(product.name, product.price)
+
+console.log(message);
+
+const heading1 = document.querySelector('h1');
