ad5d3a47499bc8fb8e562ce71529ba53ff25a03a
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-17 16:21:05 +0100

    Typescript Union

--- helloworld/app.ts
+++ helloworld/app.ts
@@ -309,3 +309,20 @@ function raiseError(msg: string): never {
     }
     return raiseError('Invalid type');
   }
+
+
+/////////////////// UNION TYPE //////////
+let reZult: number | string;
+reZult = 10; // OK
+reZult = 'Hi'; // also OK
+reZult = false; // a boolean value, not OK
+
+function add(a: number | string, b: number | string) :  number | string {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+    if (typeof a === 'string' && typeof b === 'string') {
+        return a.concat(b);
+    }
+    throw new Error('Parameters must be numbers or strings');
+}
